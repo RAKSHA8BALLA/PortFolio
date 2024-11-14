48e9545d873f5ea1f81348f27af041ec9b5e0c13
Raksha Balla <91195592+RAKSHA8BALLA@users.noreply.github.com>
2024-11-14 23:18:13 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,281 @@
+export const Bio = {
+  name: "Raksha Balla",
+  roles: ["Programmer!", "Frontend Developer!", "AI/ML Enthusiast!"],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/RAKSHA8BALLA",
+  resume:
+    "https://drive.google.com/file/d/1EK7CmEobQY4NYFCvU9BrBksa0LErjBYL/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/raksha-balla-62491b22a/",
+  twitter: "https://x.com/BallaRaksha",
+  insta: "https://www.instagram.com/rakshaballa/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Machine Learning",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Statistics",
+        image:
+          "https://jingwen-z.github.io/images/20181011-nested-pie-chart.png",
+      },
+      {
+        name: "Data visualization",
+        image: "https://cdn-icons-png.flaticon.com/512/2472/2472283.png",
+      },
+      {
+        name: "Neural network",
+        image:
+          "https://icons.veryicon.com/png/o/miscellaneous/color-work-icon/artificial-neural-network.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image: "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/v2/C4D0BAQGh716cqJsNaQ/company-logo_200_200/company-logo_200_200/0/1630537688188/branding_cataly_logo?e=2147483647&v=beta&t=zQz-CdALEUY8B_8agBfzJNJtVABHxZnR2se1WhdptpY",
+    role: "Web Development Intern",
+    company: "Branding Catalyst",
+    date: "Sept 2023 - Nov 2023",
+    desc: " Developed a comprehensive client management system that streamlines client interactions, optimizes data storage and retrieval, and delivers a seamless, responsive user experience.",
+    skills: [
+      "React.js",
+      "MongoDB",
+      "JavaScript",
+      "TypeScript",
+      "Node Js",
+      "Next Js",
+    ],
+  },
+  {
+    id: 0,
+    img: "https://media.istockphoto.com/id/1217624952/vector/work-from-home-line-icon.jpg?s=612x612&w=0&k=20&c=6RwfJ5rJbYMCQK8O4JQzCJa1hrqx8GqqBctzDaDNK4k=",
+    role: "Machine learning",
+    company: "Freelance",
+    date: "May 2023 - July 2023",
+    desc: "Achieved 85% accuracy by developing a machine learning model to predict legal petition outcomes. Integrated the model with an interactive dashboard and a recommendation system, enabling lawyers to strategize case pointseffectively.",
+    skills: [
+      "Machine Learning",
+      "Machine Learning: ",
+      "Python",
+      "Feature Engineering",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://images.collegedunia.com/public/college_data/images/logos/col28044.png",
+    school: "Rajiv Gandhi Institute of Technology",
+    date: "Oct 2020 - Sep 2024",
+    grade: "8.5 CGPA",
+    desc: "I completed my Bachelor's degree in Computer Engineering at Rajiv Gandhi Institute of Technology, Mumbai. I have completed 8 semesters and have a CGPA of 8.71. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Data Matrix Student Club (DMX) at RGIT, where I am learning and working on exciting projects with a team of talented developers.",
+    degree: "Bachelor of Engineering - BE Computer Engineering",
+  },
+  {
+    id: 1,
+    img: "https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/KCC_Mumbai_logo.svg/800px-KCC_Mumbai_logo.svg.png",
+    school: "Kishinchand Chellaram College",
+    date: "Apr 2019 - Apr 2020",
+    grade: "75%",
+    desc: "I completed my class 12 high school education at Kishinchand Chellaram College, Mumbai, where I studied Science with Computer Science.",
+    degree: "HSC(XII), Science with Computer",
+  },
+  {
+    id: 2,
+    img: "https://d3bat55ebwjhsf.cloudfront.net/schools/logos/user_generic_school_user/9b4ca6bd-2844-40f2-957d-bb399d140a54-d3a7ce88-6f_75uu2w1.webp",
+    school: "St. Anthony’s High School",
+    date: "Apr 2017 - Apr 2018",
+    grade: "88%",
+    desc: "I completed my class 10 education at St. Anthony’s High School, Mumbai.",
+    degree: "SSC(X), Science with Computer",
+  },
+];
+
+export const projects = [
+  {
+    // id: 0,
+    title: "Emotify",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "Emotify is an innovative system that captures users' emotions using OpenCV and Mediapipe, then recommends music through the Spotipy library based on the detected mood. The Emotion Module uses facial recognition and a deep learning model to classify emotions, while the Music Module maps these emotions to appropriate songs from Spotify. This combination ensures a personalized music experience tailored to the user's current emotional state.",
+    image:
+      "https://ar5iv.labs.arxiv.org/html/2212.04782/assets/someresults.jpg",
+    tags: [
+      "Python",
+      "OpenCV",
+      "Machine learning",
+      "Tensorflow",
+      "Spotipy",
+      "Streamlit",
+      "Visual studio code",
+    ],
+    category: "machine learning",
+    github: "https://github.com/RAKSHA8BALLA/Emotify",
+    webapp: "https://github.com/RAKSHA8BALLA/Emotify",
+  },
+  {
+    id: 1,
+    title: "Youtube Analysis",
+    date: "Apr 2023 - May 2023",
+    description:
+      "An AI based video analytics tool That can help YouTubers optimize their video content for higher engagement and more view. The tool analyzes video's content, length, and other factors to provide recommendations on how to improve the video's performance. Additionally, the tool provides insights on audience demographics, viewer behavior, and content trends to help YouTubers make datadriven decisions on their video strategy.",
+    image:
+      "https://www.socialinsider.io/blog/content/images/2024/01/overview-kpis.webp",
+    tags: ["Python", "Power BI", "Streamlit", "Youtube API"],
+    category: "machine learning",
+    github: "https://github.com/RAKSHA8BALLA/YouT",
+    webapp: "https://github.com/RAKSHA8BALLA/YouT",
+  },
+  {
+    id: 2,
+    title: "NewzApp",
+    date: "Oct 2022 - Nov 2023 ",
+    description:
+      "The News App, built with React, HTML, CSS, JavaScript, and Bootstrap, fetches live news directly from the NewsAPI. It provides users with the latest updates across various categories through a responsive and interactive interface. This ensures an engaging and seamless news browsing experience on any device.",
+    image:
+      "https://cdn.dribbble.com/userupload/6509947/file/original-46e2a3328428f742ebc8d74e3cc3e7e4.png?resize=400x0",
+    tags: ["React Js", "HTML", "CSS", "JavaScript", "Bootstrap", "News API"],
+    category: "web app",
+    github: "https://github.com/RAKSHA8BALLA/NewzApp",
+    webapp: "https://github.com/RAKSHA8BALLA/NewzApp",
+  },
+  {
+    id: 3,
+    title: "TextUtility",
+    date: "Jan 2023 - Mar 2023",
+    description:
+      "The Text Utility app, built with React, Bootstrap, HTML, CSS, and JavaScript, offers functions to convert text to uppercase or lowercase, copy text, clear text, and remove extra spaces. It provides a clean and efficient interface for text manipulation. This tool ensures a seamless user experience for handling text formatting tasks.",
+    image:
+      "https://www.text-filter.com/Online-Professional-Text-Editor-Text-Manipulation-Tools4.gif ",
+    tags: ["reactRouter", "HTML", "CSS", "JavaScript", "React.js"],
+    category: "web app",
+    github: "https://github.com/RAKSHA8BALLA/TextUtility",
+    webapp: "https://raksha8balla.github.io/UpdateAddress/",
+  },
+  {
+    id: 4,
+    title: "Address Updater",
+    date: "Dec 2021 - Apr 2022",
+    description:
+      "The Address Updater is a web application built with React, Bootstrap, HTML, CSS, and JavaScript, designed to manage and update user addresses. It features four input sections: Flat No or Society, City, State, and Country, each with validation to ensure accurate and complete information. It also has form validation for emply or invalid input",
+    image:
+      "https://copyassignment.com/wp-content/uploads/2022/12/op_4-1024x542.jpg",
+    tags: ["MongoDB", "HTML", "CSS", "JavaScript", "React.js"],
+    category: "web app",
+    github: "https://github.com/RAKSHA8BALLA/UpdateAddress",
+    webapp: "https://raksha8balla.github.io/UpdateAddress/",
+  },
+  {
+    id: 5,
+    title: "Twitter-sentiment-analysis",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "It uses python libraries like pandas, numpy for dataframe for operations, Sklearn for model building, NLTK for NLP tasks like tokenization, stemming, lemmitization.",
+    image:
+      "https://repository-images.githubusercontent.com/455798754/491b9635-da27-47b6-979a-4bf064f2b086",
+    tags: ["Python"],
+    category: "machine learning",
+    github: "https://github.com/RAKSHA8BALLA/Twitter-sentiment-analysis",
+    webapp: "https://github.com/RAKSHA8BALLA/Twitter-sentiment-analysis",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
